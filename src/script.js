3c577eb6464b4e7602e45036209bed38e08618d8
Laura G <laura_g56@hotmail.com>
2021-03-25 10:58:44 +1100

    Replacing with custom weather icons

--- src/script.js
+++ src/script.js
@@ -62,39 +62,39 @@ dayAndTime.innerHTML = `${currentDay} ${currentDate} ${currentMonth} ${currentYe
 function getIcon(icon) {
   let iconElement = "";
   if (icon === "03d" || icon === "03n") {
-    iconElement = "images/icons8-clouds-64.png";
+    iconElement = "images/cloudy.svg"; //scattered clouds day/night
   } else if (icon === "04d") {
-    iconElement = "images/icons8-clouds-64.png";
+    iconElement = "images/partly-cloudy.svg"; //broken clouds day
   } else if (icon === "04n") {
-    iconElement = "images/icons8-clouds-64.png";
+    iconElement = "images/night-partly-cloudy.svg"; //broken clouds night
   } else if (icon === "01d") {
-    iconElement = "images/icons8-sun-50.png";
+    iconElement = "images/sunny.svg"; //clear day
   } else if (icon === "01n") {
-    iconElement = "images/icons8-moon-dark-64.png";
+    iconElement = "images/night-clear.svg"; //clear night
   } else if (icon === "02d") {
-    iconElement = "images/icons8-partly-cloudy-day-64.png";
+    iconElement = "images/partly-cloudy.svg"; //partly cloudy day
   } else if (icon === "02n") {
-    iconElement = "images/icons8-partly-cloudy-day-64.png";
+    iconElement = "images/night-partly-cloudy.svg"; //partly cloudy night
   } else if (icon === "09d") {
-    iconElement = "images/icons8-rain-64.png";
+    iconElement = "images/sun-shower.svg"; //showers day
   } else if (icon === "09n") {
-    iconElement = "images/icons8-rain-64.png";
+    iconElement = "images/night-rainy.svg"; //showers night
   } else if (icon === "10d") {
-    iconElement = "images/icons8-rain-64.png";
+    iconElement = "images/heavy-rain.svg"; //mod-heavy rain day
   } else if (icon === "10n") {
-    iconElement = "images/icons8-rain-64.png";
+    iconElement = "images/night-rainy.svg"; //rain night
   } else if (icon === "11d") {
-    iconElement = "images/icons8-storm-64.png";
+    iconElement = "images/stormy.svg";  //thunderstorm day
   } else if (icon === "11n") {
-    iconElement = "images/icons8-storm-64.png";
+    iconElement = "images/night-storm.svg"; //thunderstorm night
   } else if (icon === "13d") {
-    iconElement = "images/icons8-snow-64.png";
+    iconElement = "images/snow.svg"; //snow day
   } else if (icon === "13n") {
-    iconElement = "images/icons8-snow-64.png";
+    iconElement = "images/night-snow.svg"; //snow night
   } else if (icon === "50d") {
-    iconElement = "images/icons8-haze-50.png";
+    iconElement = "images/haze.svg"; //mist day
   } else if (icon === "50n") {
-    iconElement = "images/icons8-haze-50.png";
+    iconElement = "images/haze.svg"; //mist night
   }
   return iconElement;
 }
